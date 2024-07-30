7e4be674ecdda51d4133f7d6a29e60da03173e8d
SquateDev <sosantyan198@gmail.com>
2024-07-30 21:19:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,8 @@
 from colorama import Fore, Style
 import os, time
 
-def menu():
- print(Fore.WHITE+"[ "+Fore.RED+"1"+Fore.WHITE+" ] : IpLogger");
- print(Fore.WHITE+"[ "+Fore.GREEN+"2"+Fore.WHITE+" ] : Phone Info");
- print(Fore.WHITE+"[ "+Fore.YELLOW+"3"+Fore.WHITE+" ] : IP-Location");
- print(Fore.WHITE+"[ "+Fore.BLUE+"4"+Fore.WHITE+" ] : CheckWeather");
- print(Fore.WHITE+"[ "+Fore.GREEN+"5"+Fore.WHITE+" ] : Exit");
- choice_input = input(Fore.YELLOW+"\nSelect Number : "+Fore.BLUE);
-def icon():
-	os.system("clear");
-	time.sleep(1);
-	print("Loading [ "+Fore.GREEN+"█"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"██"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"███"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"████"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"█████"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"██████"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"███████"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"████████"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	print("Loading [ "+Fore.GREEN+"█████████"+Fore.WHITE+" ]");
-	time.sleep(1);
-	os.system("clear");
-	icon_img = """
+def krest():
+ icon_img = """
          ██
          ██
      ██████████
@@ -48,7 +12,58 @@ def icon():
          ██
          ██
 	""";
-	print(Fore.RED+icon_img);
+ print(Fore.RED+icon_img);
+
+def IpLogger():
+ os.system("clear");
+ time.sleep(0.9);
+ krest();
+ print("Ip Logger");
+
+
+def menu():
+ print(Fore.WHITE+"[ "+Fore.RED+"1"+Fore.WHITE+" ] : IpLogger");
+ print(Fore.WHITE+"[ "+Fore.GREEN+"2"+Fore.WHITE+" ] : Phone Info");
+ print(Fore.WHITE+"[ "+Fore.YELLOW+"3"+Fore.WHITE+" ] : IP-Location");
+ print(Fore.WHITE+"[ "+Fore.BLUE+"4"+Fore.WHITE+" ] : CheckWeather");
+ print(Fore.WHITE+"[ "+Fore.GREEN+"5"+Fore.WHITE+" ] : Exit");
+ choice_input = input(Fore.YELLOW+"\nSelect Number : "+Fore.BLUE);
+ if "1" in choice_input.lower():
+  IpLogger();
+ if "2" in choice_input.lower():
+  PhoneInfo();
+
+def icon():
+	os.system("clear");
+	time.sleep(0.4);
+	print("Loading [ "+Fore.GREEN+"█"+Fore.WHITE+" ]");
+	time.sleep(0.3);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"██"+Fore.WHITE+" ]");
+	time.sleep(0.9);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"███"+Fore.WHITE+" ]");
+	time.sleep(0.7);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"████"+Fore.WHITE+" ]");
+	time.sleep(0.3);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"█████"+Fore.WHITE+" ]");
+	time.sleep(0.8);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"██████"+Fore.WHITE+" ]");
+	time.sleep(2);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"███████"+Fore.WHITE+" ]");
+	time.sleep(0.2);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"████████"+Fore.WHITE+" ]");
+	time.sleep(0.3);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"█████████"+Fore.WHITE+" ]");
+	time.sleep(0.5);
+	os.system("clear");
+	krest();
 	menu();
 
 icon();
