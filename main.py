c34e3e472834b06c31b0e4a94ec2c6605155dc06
SquateDev <sosantyan198@gmail.com>
2024-07-30 21:14:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import os, time
 def menu():
  print(Fore.WHITE+"[ "+Fore.RED+"1"+Fore.WHITE+" ] : IpLogger");
  print(Fore.WHITE+"[ "+Fore.GREEN+"2"+Fore.WHITE+" ] : Phone Info");
- choice_input = input(Fore.YELLOW+"Select Number :"+Fore.BLUE);
+ print(Fore.WHITE+"[ "+Fore.YELLOW+"3"+Fore.WHITE+" ] : IP-Location");
+ print(Fore.WHITE+"[ "+Fore.BLUE+"4"+Fore.WHITE+" ] : CheckWeather");
+ print(Fore.WHITE+"[ "+Fore.PURPLE+"5"+Fore.WHITE+" ] : Exit"):
+ choice_input = input(Fore.YELLOW+"\nSelect Number : "+Fore.BLUE);
 def icon():
 	os.system("clear");
 	time.sleep(1);
