0e6f6df7b93314f3a48f22922836416916e4db58
SquateDev <sosantyan198@gmail.com>
2024-07-30 21:03:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 from colorama import Fore, Style
-
+import os, time
 def icon():
+	os.system("clear");
+	time.sleep(1);
+	print("Loading [ "+Fore.GREEN+"█"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"██"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"███"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"████"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"█████"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"██████"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"███████"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"████████"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
+	print("Loading [ "+Fore.GREEN+"█████████"+Fore.WHITE+" ]");
+	time.sleep(1);
+	os.system("clear");
 	icon_img = """
-  ░░░██░░░
-  ░░░██░░░
-  ████████
-  ░░░██░░░
-  ░░░██░░░
-  ░░░██░░░
-  ░░░██░░░
-  ░░░██░░░
+         ██
+         ██
+     ████████
+         ██
+         ██
+         ██
+         ██
+         ██
 	""";
 	print(Fore.RED+icon_img);
 
