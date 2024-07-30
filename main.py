0cd78d97fd8d1a770f83f70752a85bfa2dacd86d
SquateDev <sosantyan198@gmail.com>
2024-07-30 20:54:24 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from colorama import Fore, Style
+
+def icon():
+	icon_img = """
+  ░░░██░░░
+  ░░░██░░░
+  ████████
+  ░░░██░░░
+  ░░░██░░░
+  ░░░██░░░
+  ░░░██░░░
+  ░░░██░░░
+	""";
+	print(Fore.RED+icon_img);
+
+icon();
